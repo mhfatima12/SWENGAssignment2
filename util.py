ed17146395fc3a99ce9560a5e2c37b37c020179d
Max Cunningham <maxzinedine@gmail.com>
2022-10-21 09:57:13 +0100

    Add utilities to project

---
+++ util.py
@@ -0,0 +1,41 @@
+import math as math
+
+
+# isNumber takes a string c and checks if the given string is a float or integer.
+def isNumber(c: str):
+    for i in range(len(c)):
+        if not (str.isnumeric(c[i]) or c[i] == '.'):
+            return False
+    return True
+
+
+operators = ['+', '-', '/', '*', '^']
+
+
+# isOperator checks if the given string is an operator.
+def isOperator(c: str):
+    if len(c) != 1:
+        return False
+
+    for i in range(len(operators)):
+        if c == operators[i]:
+            return True
+    return False
+
+
+# perform takes an operaton and returns the output of this operation.
+def perform(val1: float|int, op: str|int, val2: float|int):
+    match op:
+        case '+':
+            return val1 + val2
+        case '-':
+            return val1 - val2
+        case '*':
+            return val1 * val2
+        case '/':
+            if val2 == 0.0:
+                return "divide by zero"
+            return val1 / val2
+        case '^':
+            return math.pow(val1, val2)
+    return "could not perform operation: " + str(val1) + op + str(val2)
---
+++ util_test.py
@@ -0,0 +1,66 @@
+import util as util
+
+
+def test_isNumber() -> None:
+    tests = [
+        ('1', True),
+        ('1.345', True),
+        ('90857.592', True),
+        ('abcd', False),
+        ('13759a.039', False),
+        ('124904,3402', False),
+    ]
+
+    hasFail = False
+    for test in tests:
+        result = util.isNumber(test[0])
+        if result is not test[1]:
+            hasFail = True
+            print("isNumber("+test[0]+") expected: " +
+                  str(test[1])+" got: "+str(result))
+    assert not hasFail
+
+
+def test_isOperator() -> None:
+    tests = [
+        ('1', False),
+        ('++', False),
+        ('+', True),
+        ('-', True),
+        ('/', True),
+        ('*', True),
+        ('^', True),
+        ('a', False),
+        ('a+c', False),
+    ]
+
+    hasFail = False
+    for test in tests:
+        result = util.isOperator(test[0])
+        if result != test[1]:
+            hasFail = True
+            print("isOperator("+str(test[0])+") expected: " +
+                  str(test[1])+" got: "+str(result))
+    assert not hasFail
+
+
+def test_perform() -> None:
+    tests = [
+        (1, '+', 2, 3),
+        (2, '-', 5, -3),
+        (5, '-', 2, 3),
+        (3, '^', 2, 9),
+        (5, '/', 0, 'divide by zero'),
+        (4, '*', 2, 8),
+        (1.5, '-', 0.5, 1.0),
+        (345, '***', 7, 'could not perform operation: 345***7')
+    ]
+
+    hasFail = False
+    for test in tests:
+        result = util.perform(test[0], test[1], test[2])
+        if result != test[3]:
+            hasFail = True
+            print("perform("+str(test[0])+test[1]+str(test[2])+") expected: " +
+                  str(test[3])+" got: "+str(result))
+    assert not hasFail
