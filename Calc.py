7372e2690ac47785f767c1dc284ed3e051673d17
Haojun6 <99478305+Haojun6@users.noreply.github.com>
2022-11-03 19:35:56 +0000

    semi-final fits brackets and other functions
    need to fit negative input

--- Calc.py
+++ Calc.py
@@ -1,8 +1,8 @@
-from typing import List
-import string as string
-
-OPERATORS = ('+', '-', '*')
+from decimal import *
+import math
+OPERATORS = ('+', '-', '*', '/', '^', '(', ')', '.', 'l', 'o', 'g', 'e', 'x', 'p')
 DIGITS = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+stack = []
 
 
 def is_valid(lst):
@@ -13,75 +13,179 @@ def is_valid(lst):
 
 
 def calc(lst):
-    number_list = []
+    bracket(lst, 0)
+    while stack.__len__()>1 or stack.__len__()==0:
+
+        new_list = stack.copy()
+        stack.clear()
+        bracket(new_list, 0)
+    return stack[0]
+
+
+def classify(lst):
+    number_list=[]
     operator_list = []
     number = ''
     for i in lst:
-        if is_digit(i):
+        if not is_operator(i) or i == '.':
             number = number + i
         else:
-            number_list.append(int(number))
+            number_list.append(number)
             operator_list.append(i)
             number = ''
-    number_list.append(int(number))
-    operator_list, number_list = mul(operator_lst=operator_list, number_lst=number_list)
-    operator_list, number_list = add_sub(operator_lst=operator_list, number_lst=number_list)
-    return number_list[0]
+    number_list.append(number)
+    return number_list, operator_list
 
 
-def mul(operator_lst: List, number_lst: List):
+def operation(number_list, operator_list):
+    try:
+        log_exp(number_list, operator_list)
+        power(number_list, operator_list)
+        mul_div(number_list, operator_list)
+        add_sub(number_list, operator_list)
+    except ZeroDivisionError:
+        print("Zero can't be divided")
+        quit()
+    except InvalidOperation:
+        print("Invalid input")
+        quit()
+    return number_list, operator_list
+
+
+def bracket(lst, count):
+    if '(' not in lst:
+        number_list = classify(lst)[0]
+        operator_list = classify(lst)[1]
+        if stack:
+            stack.pop()
+        stack.append(str(operation(number_list, operator_list)[0][0]))
+        if '(' in stack:
+            stack.append(')')
+        return
+    else:
+        i = 0
+        while i < lst.__len__():
+            stack.append(lst[i])
+            if str(lst[i]) == '(':
+                count = count + 1
+                nlst = []
+                i += 1
+                while  count != 0:
+                    if str(lst[i]) == '(':
+                        count+=1
+                    if str(lst[i]) == ')':
+                        count-=1
+                    if count == 0:
+                        break
+                    nlst.append(lst[i])
+                    i +=1
+                bracket(nlst,count)
+            i +=1
+
+
+def log_exp(number_list, operator_list):
     i = 0
-    while i < operator_lst.__len__():
-        if operator_lst[i] == '*':
-            product = number_lst[i] * number_lst[i + 1]
-            operator_lst.pop(i)
-            number_lst.pop(i)
-            number_lst.pop(i)
-            number_lst.insert(i, product)
+    while i < operator_list.__len__():
+        if operator_list[i] == 'l':
+            if operator_list[i+1] == 'o':
+                if operator_list[i+2] == 'g':
+                    operator_list.pop(i)
+                    operator_list.pop(i)
+                    operator_list.pop(i)
+                    number_list.pop(i)
+                    number_list.pop(i)
+                    number_list.pop(i)
+                    number_list[i] = Decimal(str(math.log(float(number_list[i]))))
+                else:
+                    print('error')
+                    break
+            else:
+                print('error')
+                break
+            i -= 1
+        elif operator_list[i] == 'e':
+            if operator_list[i+1] == 'x':
+                if operator_list[i+2] == 'p':
+                    operator_list.pop(i)
+                    operator_list.pop(i)
+                    operator_list.pop(i)
+                    number_list.pop(i)
+                    number_list.pop(i)
+                    number_list.pop(i)
+                    number_list[i] = Decimal(str(math.exp(float(number_list[i]))))
+                else:
+                    print('error')
+                    break
+            else:
+                print('error')
+                break
             i -= 1
         i += 1
-    return operator_lst, number_lst
 
 
-def add_sub(operator_lst: List, number_lst: List):
-    for i in range(1, number_lst.__len__()):
-        number1 = number_lst[0]
-        number2 = number_lst[1]
-        if operator_lst[0] == '+':
-            result = number1 + number2
-        elif operator_lst[0] == '-':
-            result = number1 - number2
-        operator_lst.pop(0)
-        number_lst.pop(1)
-        number_lst[0] = result
-    return operator_lst, number_lst
+def power(number_list, operator_list):
+    i = 0
+    while i < operator_list.__len__():
+        if operator_list[i] == '^':
+            result = Decimal(number_list[i]) ** Decimal(number_list[i + 1])
+            operator_list.pop(i)
+            number_list.pop(i)
+            number_list.pop(i)
+            number_list.insert(i, result)
+            i -= 1
+        i += 1
 
 
-def is_digit(digit):
-    return digit in DIGITS
+def mul_div(number_list, operator_list):
+    i = 0
+    while i < operator_list.__len__():
+        if operator_list[i] == '*':
+            result = Decimal(number_list[i]) * Decimal(number_list[i + 1])
+            operator_list.pop(i)
+            number_list.pop(i)
+            number_list.pop(i)
+            number_list.insert(i, result)
+            i -= 1
+        elif operator_list[i] == '/':
+            result = Decimal(number_list[i]) / Decimal(number_list[i + 1])
+            operator_list.pop(i)
+            number_list.pop(i)
+            number_list.pop(i)
+            number_list.insert(i, result)
+            i -= 1
+        i += 1
 
 
-def remove_whitespace(sequence:str):
-    return sequence.replace(" ","")
+def add_sub(number_list, operator_list):
+    for i in range(1, number_list.__len__()):
+        number1 = number_list[0]
+        number2 = number_list[1]
+        if operator_list[0] == '+':
+            result = Decimal(number1) + Decimal(number2)
+        elif operator_list[0] == '-':
+            result = Decimal(number1) - Decimal(number2)
+        operator_list.pop(0)
+        number_list.pop(1)
+        number_list[0] = result
 
-def calculate(sequence):
-    sequence = remove_whitespace(sequence)
-    print(sequence)
+
+def is_operator(op):
+    return op in OPERATORS
+
+
+while True:
+    sequence = input("Please enter a sequence that you want to compute: (q to end)")
     se_lst = list(sequence)
-    if is_valid(se_lst):
-        try:
-            result = calc(se_lst)
-        except BaseException as e:
-            return e
-        else:
-            return result
+    if sequence == 'q':
+        break
+    elif is_valid(se_lst):
+        result = calc(se_lst)
+        stack.clear()
+        print("The result for", sequence, "is", "%.3f" % float(result))
+        again = input("Would you like to have another try? y/n")
+        if again == 'y':
+            continue
+        elif again == 'n':
+            break
     else:
-        return "Error: The sequence must only include operators and digits"
-
-
-# while True:
-#     sequence = input("Please enter a sequence that you want to compute: (q to end)")
-#     if sequence == 'q':
-#         break
-#     else:
-#         calculate(sequence)
+        print("Error:The sequence must only include operators and digits")
