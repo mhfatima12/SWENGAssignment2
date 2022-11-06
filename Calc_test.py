ce435c8fc7982b52fd2ec3c69a9b0a22c987381a
mfatima <mfatima@tcd.ie>
2022-11-06 06:09:02 +0000

    is_operator function tested

--- Calc_test.py
+++ Calc_test.py
@@ -35,3 +35,35 @@ def test_is_valid() -> None:
             print("is_valid("+str(test[0])+") expected: " +
                   str(test[1])+" got: "+str(result))
     assert not hasFail
+
+
+def test_is_operator() -> None:
+    tests = [
+        #('++', False), #currently failing
+        ('+', True),
+        ('-', True),
+        ('/', True),
+        ('*', True),
+        ('^', True),
+        ('a', False),
+        ('log', True),
+        #('Log', True), #currently failing
+        #('EXP', True), #currently failing
+        #('ExP', True), #currently failing
+        ('exp', True),
+        ('expres', False),
+        ('logs', False),
+        ('(', True),
+        (')', True)
+    ]
+
+    hasFail = False
+    for test in tests:
+        result = Calc.is_operator(test[0])
+        if result != test[1]:
+            hasFail = True
+            print("is_valid("+str(test[0])+") expected: " +
+                  str(test[1])+" got: "+str(result))
+    assert not hasFail
+
+    
