a58b167da50fe749ed1a5ced7ef31404bdd44a74
mfatima <mfatima@tcd.ie>
2022-11-07 11:33:58 +0000

    indentation fixed

--- Calc_test.py
+++ Calc_test.py
@@ -111,9 +111,9 @@ def test_calc() -> None:
     assert not hasFail
 
 # Returns true if 100 / 0 raises an Exception
-   def test_calc2(self):
-      with self.assertRaises(ZeroDivisionError):
-         '100 / 0'
+def test_calc2(self):
+    with self.assertRaises(ZeroDivisionError):
+        '100 / 0'
   
 if __name__ == '__main__': 
     unittest.main()
