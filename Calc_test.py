8feb2a62a0fcd97790196ebd45295b94288ace91
mfatima <mfatima@tcd.ie>
2022-11-07 12:44:23 +0000

    division by zero

--- Calc_test.py
+++ Calc_test.py
@@ -113,5 +113,5 @@ def test_calc() -> None:
     assert not hasFail
 
 def test_calc2():
-    with pytest.raises(decimal.DivisonByZero):
+    with pytest.raises(ZeroDivisionError):
         Calc.mul_div(['100', '0'], ['/'])
